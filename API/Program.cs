887d48e628debc0cc9064f461b49a8fc000cc3b8
Aristo <xr564178@gmail.com>
2026-02-19 22:54:50 +0200

    end of stage 2

--- API/Program.cs
+++ API/Program.cs
@@ -15,7 +15,7 @@ builder.Services.AddDbContext<AppDbContext>(opt =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-
+  
 app.MapControllers();
 
 
@@ -33,4 +33,7 @@ catch (Exception ex)
     var logger = service.GetRequiredService<ILogger<Program>>();
     logger.LogError(ex,"An error occurred during migration.");
 }
+
+
+Console.WriteLine("Starting API...");
 app.Run();
