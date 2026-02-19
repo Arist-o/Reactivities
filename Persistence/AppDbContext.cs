3fe51aaf2f95c8fc5f18ebf186a4c0a7c0088bcf
Aristo <xr564178@gmail.com>
2026-02-19 22:53:19 +0200

    Add project files.

---
+++ Persistence/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence
+{
+    public class AppDbContext(DbContextOptions options) : DbContext(options)
+    {
+        public required DbSet<Activity> Activities { get; set; }
+    }
+}
