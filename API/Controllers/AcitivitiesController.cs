6841e0b5396838d1b6140b7ae219d7afab155083
Aristo <xr564178@gmail.com>
2026-03-01 11:43:41 +0200

    end of stage 4

--- API/Controllers/AcitivitiesController.cs
+++ API/Controllers/AcitivitiesController.cs
@@ -1,26 +1,43 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using Persistence;
+
 using Domain;
-using Microsoft.EntityFrameworkCore;
-using System.Runtime.CompilerServices;
+using MediatR;
+using Application.Activities.Queries;
+
 namespace API.Controllers
 {
-    public class ActivitiesController(AppDbContext context) : BaseApiController
+    public class ActivitiesController : BaseApiController
     {
         [HttpGet]
-        public async Task<ActionResult<List<Activity>>> GetActivities() {
-            return await context.Activities.ToListAsync();
+        public async Task<ActionResult<List<Activity>>> GetActivities()
+        {
+            return await Mediator.Send(new GetActivityList.Query());
         }
 
         [HttpGet("{id}")]
-        public async Task<ActionResult<Activity>> GetActivityDetail(string id){
-           var activity = await context.Activities.FindAsync(id);
+        public async Task<ActionResult<Activity>> GetActivityDetail(string id)
+        {
+            return await Mediator.Send(new GetActivityDetails.Query { Id = id });
+        }
 
-            if (activity == null)
-            {
-                return NotFound();
-            }
-            return activity;
+        [HttpPost]
+        public async Task<ActionResult<string>> CreateActivity(Activity activity)
+        {
+            return await Mediator.Send(new Application.Activities.Commands.CreateActivity.Command { Activity = activity });
+        }
+
+        [HttpPut]
+        public async Task<ActionResult> EditActivity(Activity activity)
+        {
+            await Mediator.Send(new Application.Activities.Commands.EditActivity.Command { Activity = activity });
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> DeleteActivity(string id)
+        {
+            await Mediator.Send(new Application.Activities.Commands.DeleteActivity.Command { Id = id });
+            return Ok();
         }
     }
 }
---
+++ Application/Core/MappingProfiles.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application.Core
+{
+    public class MappingProfiles : Profile
+    {
+        public MappingProfiles() 
+        {
+            CreateMap<Domain.Activity, Domain.Activity>();
+        }
+    }
+}
